97fd779853c851b8ebb303934144065af09438a8
DHRITI19 <76477697+DHRITI19@users.noreply.github.com>
2022-04-27 15:07:24 +0530

    Add files via upload

---
+++ normalDsit.py
@@ -0,0 +1,27 @@
+import statistics
+import random
+import plotly.figure_factory as ff
+import csv
+import pandas as pd
+
+df = pd.read_csv("StudentsPerformance.csv")
+data = df["reading score"].tolist
+mean = sum(data) / len(data)
+median = statistics.median(data)
+mode = statistics.mode(data)
+std_deviation = statistics.stdev(data)
+first_std_deviation_start, first_std_deviation_end = mean-std_deviation, mean+std_deviation
+second_std_deviaiton_start, second_std_deviation_end = mean-(2*std_devation), mean+(2*std_devation)
+third_std_devation_start, third_std_deviation_end = mean-(3*std_devation), mean+(3*std_devation)
+
+thin_1_std_devation = [result for result in data if result > first_std_devation_start and result < first_std_deviation_end]
+thin_2_std_devation = [result for result in data if result > second_std_devation_start and result < second_std_deviation_end]
+thin_3_std_devation = [result for result in data if result > third_std_devation_start and result < third_std_deviation_end]
+print("{}% of data lies within one standard deviation".format(len(list_of_data_within_1_std_deviation)*100.0/len(dice_result)))
+print("Standard deviation of this data is {}".format(std_deviation))
+print("Mean of this data is {}".format(mean))
+print("Median of this data is {}".format(median))
+print("Mode of this data is {}".format(mode))
+
+fig = ff.create_distplot([dice_result], ["score"], show_hist = False)
+fig.show()
